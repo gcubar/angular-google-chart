743c773c6fdc76e62e543c45d3f2bfe4a60c0231
Gilberto Cuba Ricardo <gcubar2005@gmail.com>
2018-04-19 19:27:14 -0400

    . when we have one initial definition, and then assign data values its finish with three chart ready counter

--- partials/generic.js
+++ partials/generic.js
@@ -1,57 +1,65 @@
 /* global angular */
-angular.module("google-chart-sample").controller("GenericChartCtrl", function ($scope, $routeParams) {
-    $scope.chartObject = {};
+angular.module("google-chart-sample").controller("GenericChartCtrl",
+    ['$scope', '$routeParams', '$timeout', function ($scope, $routeParams, $timeout) {
+        $scope.chartObject = {};
 
-    $scope.locker = {
-        lockDraw: function() {  },
-        unLockDraw: function() {  },
-        isDrawLocked: function() { return false; }
-    };
+        $scope.locker = {
+            lockDraw: function() {  },
+            unLockDraw: function() {  },
+            isDrawLocked: function() { return false; }
+        };
 
-    $scope.locker.lockDraw();
 
-    $scope.onions = [
-        {v: "Onions"},
-        {v: 3},
-    ];
-
-    $scope.chartObject.data = {"cols": [
-        {id: "t", label: "Topping", type: "string"},
-        {id: "s", label: "Slices", type: "number"}
-    ], "rows": [
-        {c: [
-            {v: "Mushrooms"},
+        $scope.onions = [
+            {v: "Onions"},
             {v: 3},
-        ]},
-        {c: $scope.onions},
-        {c: [
-            {v: "Olives"},
-            {v: 31}
-        ]},
-        {c: [
-            {v: "Zucchini"},
-            {v: 1},
-        ]},
-        {c: [
-            {v: "Pepperoni"},
-            {v: 2},
-        ]}
-    ]};
+        ];
 
+        // $routeParams.chartType == BarChart or PieChart or ColumnChart...
+        $scope.chartObject.type = $routeParams.chartType;
+        $scope.chartObject.options = {
+            'title': 'How Much Pizza I Ate Last Night'
+        };
 
-    // $routeParams.chartType == BarChart or PieChart or ColumnChart...
-    $scope.chartObject.type = $routeParams.chartType;
-    $scope.chartObject.options = {
-        'title': 'How Much Pizza I Ate Last Night'
-    };
+        $scope.chartObject.data = {
+            "cols": [
+                {id: "t", label: "Topping", type: "string"},
+                {id: "s", label: "Slices", type: "number"}
+            ]
+        };
 
-    $scope.chartReadyCounter = 0;
+        $timeout(function() {
+            $scope.locker.lockDraw();
 
-    $scope.chartReady = function() {
-        $scope.chartReadyCounter++;
-        console.log('GENERIC - chart ready counter = ' + $scope.chartReadyCounter);
-    };
+            $scope.chartObject.data.rows = [
+                {c: [
+                        {v: "Mushrooms"},
+                        {v: 3}
+                    ]},
+                {c: $scope.onions},
+                {c: [
+                        {v: "Olives"},
+                        {v: 31}
+                    ]},
+                {c: [
+                        {v: "Zucchini"},
+                        {v: 1},
+                    ]},
+                {c: [
+                        {v: "Pepperoni"},
+                        {v: 2},
+                    ]}
+            ];
 
-    $scope.locker.unLockDraw();
+            $scope.locker.unLockDraw();
+        }, 5000);
 
-});
+        $scope.chartReadyCounter = 0;
+
+        $scope.chartReady = function() {
+            $scope.chartReadyCounter++;
+            console.log('GENERIC - chart ready counter = ' + $scope.chartReadyCounter);
+        };
+
+    }
+]);

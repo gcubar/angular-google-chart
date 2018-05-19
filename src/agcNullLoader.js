36e6ee05badd17d3bb1af0117bc4f0c1af5820f7
Gabriel <teloconf@gmail.com>
2018-05-19 23:17:21 +0200

    Update agcNullLoader.js

--- src/agcNullLoader.js
+++ src/agcNullLoader.js
@@ -10,14 +10,16 @@
     function AgcNullLoaderProvider(){
         this._hasTrigger = false;
         this._libraryOverride = null;
-        this._triggerFunction = (function(){
-            // If the trigger function is called before $get,
-            // just act as if it was never fetched.
-            if (this._deferred)
-                this._deferred.resolve(this._libraryOverride || google);
-            else
-                this._hasTrigger = false;
-        }).bind(this);
+        if(this._triggerFunction != null && this._triggerFunction != undefined){
+           this._triggerFunction = (function(){
+                // If the trigger function is called before $get,
+                // just act as if it was never fetched.
+                if (this._deferred)
+                    this._deferred.resolve(this._libraryOverride || google);
+                else
+                    this._hasTrigger = false;
+            }).bind(this);
+        }        
         this._deferred = null;
     }
 
